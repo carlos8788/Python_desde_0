f7fd7c081126e8a26966bc08ab0395f8251413e8
carlos8788 <carlos8788@gmail.com>
2023-01-08 19:53:57 -0300

    Ejercicios

---
+++ ejercicios_1/ejercicio_1_1.py
@@ -0,0 +1,6 @@
+print(range(0,4))
+x = range(4)
+print(x)
+
+for i in range(4):
+    print(i)
---
+++ ejercicios_1/ejercicio_1_2.py
@@ -0,0 +1,6 @@
+# Pedirle al usuario que ingrese 2 número enteros y que devuelva la suma, la multiplicación
+# la resta y division
+
+# una pequeña ayuda
+numero_a = int(input("Ingrese un número entero: "))
+
